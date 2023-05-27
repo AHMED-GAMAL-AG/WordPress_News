a2af7f609dbda86e58987af09a4f453c67ad04ca
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-05-27 14:53:33 +0300

    10. عرض الأخبار على حسب التصنيف

--- wp-content/themes/news_theme/archive.php
+++ wp-content/themes/news_theme/archive.php
@@ -1,51 +1,53 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package news_app
+ * @package News-Hsoub
  */
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main>
+	<section class="news mt-5">
+		<div class="container">
+			<div class="row">
+				<h3 class="my-5">جميع الأخبار العائدة لتصنيف <?php the_category('،') ?></h3>
+			</div>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+			<div class="row">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				while (have_posts()) {
+					the_post();
 				?>
-			</header><!-- .page-header -->
+					<div class="col-lg-3 col-md-4 col-sm-6 mt-4 mb-5">
+						<a href="<?php the_permalink(); ?>" class="article-link">
+							<?php if (has_post_thumbnail()) { ?>
+								<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" />
+							<?php
+							} else { ?>
+								<img src="<?php bloginfo('template_directory'); ?>/images/default.jpg" alt="<?php the_title(); ?>" />
+							<?php } ?>
+						</a>
+						<div class="article-text mt-3">
+							<span class="article-category">
+								<?php the_category('،') ?>
+							</span>
+							<h5 class="article-title article-link pb-4 text-dark">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h5>
+						</div>
+					</div>
+				<?php } ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php
-get_sidebar();
+
 get_footer();
